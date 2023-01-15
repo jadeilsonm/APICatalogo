f3a144dd0ae92cfa922bde3c2ea877eaab4fac86
jadeilson Martinho <jadeilsonm17@gmail.com>
2023-01-15 15:22:37 -0300

    feat: create migrations to insert data

---
+++ APICatalogo/Migrations/20230115175941_updateNameTable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateNameTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Categories_CategoryId",
+                table: "Produtos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Produtos",
+                table: "Produtos");
+
+            migrationBuilder.RenameTable(
+                name: "Produtos",
+                newName: "Products");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Produtos_CategoryId",
+                table: "Products",
+                newName: "IX_Products_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Produtos");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoryId",
+                table: "Produtos",
+                newName: "IX_Produtos_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Produtos",
+                table: "Produtos",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Categories_CategoryId",
+                table: "Produtos",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20230115181158_insertCategories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class insertCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Bebidas','bebidas.jpg')");
+            migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Lanches','lanches.jpg')");
+            migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Sobremesas','sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}
